ea4dc25bf3a16f99b44310c465d8d4563db11009
Denys Prykhodko <Denys_Prykhodko@epam.com>
2020-02-14 15:06:06 +0200

    asdsa

--- tasks/Task1.1/src/main/java/com/epam/prykhodko/command/Command.java
+++
@@ -1,5 +0,0 @@
-package com.epam.prykhodko.command;
-
-public interface Command {
-  void execute();
-}
--- tasks/Task1.1/src/main/java/com/epam/prykhodko/util/FileCompress.java
+++
@@ -1,9 +0,0 @@
-package com.epam.prykhodko.util;
-
-import java.io.File;
-
-public interface FileCompress {
-  void write(File input, File output);
-
-  void read(File input, File output);
-}
