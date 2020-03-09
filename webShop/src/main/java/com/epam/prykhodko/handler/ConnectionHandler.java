f59ef2dadb46dba438eafba3474478cdda722d83
Denys Prykhodko <Denys_Prykhodko@epam.com>
2020-03-09 17:00:08 +0200

    add some messages

--- webShop/src/main/java/com/epam/prykhodko/handler/ConnectionHandler.java
+++ webShop/src/main/java/com/epam/prykhodko/handler/ConnectionHandler.java
@@ -1,6 +1,8 @@
 package com.epam.prykhodko.handler;
 
 import static com.epam.prykhodko.constants.DBConstants.DB_URL;
+import static com.epam.prykhodko.constants.LoggerMessagesConstants.ERR_CANNOT_CREATE_INITIAL_CONTEXT;
+import static com.epam.prykhodko.constants.LoggerMessagesConstants.ERR_CANNOT_OBTAIN_CONNECTION;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -8,9 +10,11 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import org.apache.log4j.Logger;
 
 public class ConnectionHandler {
 
+    private static final Logger LOGGER = Logger.getLogger(ConnectionHandler.class);
     private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
 
     public static void setConnection() {
@@ -22,9 +26,9 @@ public class ConnectionHandler {
             con.setAutoCommit(false);
             threadLocal.set(con);
         } catch (SQLException e) {
-            //TODO
+            LOGGER.error(ERR_CANNOT_OBTAIN_CONNECTION);
         } catch (NamingException e) {
-            //TODO
+            LOGGER.error(ERR_CANNOT_CREATE_INITIAL_CONTEXT);
         }
     }
 
